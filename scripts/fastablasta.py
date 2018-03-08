716f406cf6f5f3531720db36c1cc8f3629d39386
mjobin <mjobin@gmail.com>
2018-03-08 13:51:52 -0800

    Multiple file handling for comparisons.

--- scripts/fastablasta.py
+++ scripts/fastablasta.py
@@ -9,6 +9,7 @@
 import argparse
 from argparse import RawTextHelpFormatter
 import random
+import os
 
 def mutit(x, tstv):
 	ts = True
@@ -54,22 +55,21 @@ parser = argparse.ArgumentParser(description="This script does: \n\n\t" \
 											 "- .\n\t" \
 											 "- ",formatter_class=RawTextHelpFormatter)
 
-parser.add_argument('-file',metavar='<file>',help='output file: .fasta', required=True)
-parser.add_argument('-reffile',metavar='<reffile>',help='reference sequence file')
+# parser.add_argument('-outfile',metavar='<file>',help='output file: .fasta', required=True)
 parser.add_argument('-l', metavar='<length>', help='Sequence length.', default='1000')
 parser.add_argument('-c', metavar='<count>', help='Number of sequences.', default='1000')
-parser.add_argument('-mu', metavar='<mutrate>', help='Mutation rate.', default='8.71e-10')
+parser.add_argument('-mu', metavar='<mutrate>', help='Mutation rate.', default='0.0')
 parser.add_argument('-miss', metavar='<miss>', help='Missing rate.', default='0.0')
 parser.add_argument('-tstv', metavar='<tstv>', help='Transition/travsersion ratio.', default='2.0')
-parser.add_argument('-infile', metavar='<infile>', help='FASTA input file.')
+parser.add_argument('-infile', metavar='<infile>', help='FASTA input file.', required=True)
 parser.add_argument('-indelsize', metavar='<indelsize>', help='Max indel size', default=0)
 parser.add_argument('-indelrate', metavar='<indelrate>', help='Indel rate.', default=0.0)
 parser.add_argument('-copyrate', metavar='<copyrate>', help='Copy rate.', default=0.01)
+parser.add_argument('-reps', metavar='<reps>', help='Replicates.', default=10)
 
 
 args = parser.parse_args()
-outfile = args.file
-reffile = args.reffile
+# outfile = args.outfile
 length = int(args.l)
 count = int(args.c)
 mu = float(args.mu)
@@ -78,34 +78,41 @@ tstv = float(args.tstv)
 infile = args.infile
 idsize = int(args.indelsize)
 idrate = float(args.indelrate)
+reps = int(args.reps)
+
 
 acgt = {'A', 'C', 'G', 'T'}
 missing = {'.', '-', 'N'}
 
-workseqs = []
-ref = []
-if infile is None:
-
-	for x in range(0, length):
-		r = random.randint(0,3)
-		if r == 0:
-			ref.append("A")
-		elif r == 1:
-			ref.append("C")
-		elif r == 2:
-			ref.append("G")
-		elif r == 3:
-			ref.append("T")
-
-	theseq = ("".join(ref))
-	for y in range(0, count):
-		newseq = []
-		newseq.append(str(y))
-		newseq.append(theseq)
-		workseqs.append(newseq)
 
 
+batchlist = []
+
+if os.path.isdir(infile):
+	for bbfile in os.listdir(infile):
+		if bbfile.endswith(".fasta"):
+			batchlist.append(infile + "/" + bbfile)
+elif os.path.isfile(infile):
+	if infile.endswith(".fasta"):
+		batchlist.append(infile)
+	else:
+		print "Input file must be .fasta"
+		exit()
 else:
+	print "Input file must be fasta"
+	exit()
+
+for infile in batchlist:
+# 	inbasename = os.path.basename(afile)
+	infilebase, infileext = os.path.splitext(infile)
+
+	bases = []
+
+	workseqs = []
+	ref = []
+
+	firstline = True
+	firstseq = None
 	file_data = open(infile, 'r')
 	for file_line in file_data:
 		if file_line[:1] is '>':
@@ -115,67 +122,99 @@ else:
 			newseq.append(seqname)
 			newseq.append(seq)
 			workseqs.append(newseq)
+			if firstline:
+				firstline = False
+				firstseq = seq
+				for i in xrange(len(firstseq)):
+					bases.append(set())
+			else:	
+				if len(seq) != len(firstseq):
+					print "Error! All lines must be same length!"
+					exit()
+		
 
-# print workseqs
-outseqs = []
-#Mutation, missing data
-for seq in workseqs:
-	outseq = []
-	seq_line = seq[1]
-	out_line = []
-	tot = 0
-	missed = 0
-	for i in xrange(len(seq_line)):
-#     for base in seq_line:
-		if random.random() < miss:
-			out_line.append("-")
-			missed = missed + 1
-		elif random.random() < mu:
-			out_line.append(mutit(seq_line[i],tstv))
-		else:
-			out_line.append(seq_line[i])
-		tot = tot +1
-	for i in xrange(len(out_line)):
-		if random.random() < idrate:
-			indel = random.randint(-idsize, idsize)
-			if indel < 0:
-				for x in range(0, indel):
-					del out_line[i]
-			elif indel > 0:
-				for x in range(0, indel):
-					insb = "-"
-					r = random.randint(0,3)
-					if r == 0:
-						insb = "A"
-					elif r == 1:
-						insb = "C"
-					elif r == 2:
-						insb = "G"
-					elif r == 3:
-						insb = "T"
-					out_line.insert(i, insb)
+	#work out what bases are at which sites. only these will be errors
+	#
+
+	for seq in workseqs:
+		seq_line = seq[1]
+		for i in xrange(len(seq_line)):
+			bases[i].add(seq_line[i])
+	
+	for rep in range(reps):
+	# print workseqs
+		outseqs = []
+		#Mutation, missing data
+		for seq in workseqs:
+			outseq = []
+			seq_line = seq[1]
+			out_line = []
+			tot = 0
+			missed = 0
+			for i in xrange(len(seq_line)):
+		#     for base in seq_line:
+				if random.random() < miss:
+					out_line.append("-")
+					missed = missed + 1
+				elif random.random() < mu and len(bases[i])>1:
+					while(True):
+						muted = random.choice(tuple(bases[i]))
+						if muted is not seq_line[i]:
+							out_line.append(muted)
+							break
+		# 			out_line.append(mutit(seq_line[i],tstv))
+				else:
+					out_line.append(seq_line[i])
+				tot = tot +1
+			for i in xrange(len(out_line)):
+				if random.random() < idrate:
+					indel = random.randint(-idsize, idsize)
+					if indel < 0:
+						for x in range(0, indel):
+							del out_line[i]
+					elif indel > 0:
+						for x in range(0, indel):
+							insb = "-"
+							r = random.randint(0,3)
+							if r == 0:
+								insb = "A"
+							elif r == 1:
+								insb = "C"
+							elif r == 2:
+								insb = "G"
+							elif r == 3:
+								insb = "T"
+							out_line.insert(i, insb)
 
 
-	outseq.append(seq[0])
-	outseq.append("".join(out_line))
-	outseqs.append(outseq)
+			outseq.append(seq[0])
+			outseq.append("".join(out_line))
+			outseqs.append(outseq)
 
 
 
 
-#Write to a FASTA file so that it can be read in as a
-outfilename = outfile + ".fasta"
-outfile = open(outfilename, 'w')
-lenchk = -1
+		#Write to a FASTA file so that it can be read in as a
+		missbase = str(miss)
+		misses = missbase.split(".")
+		missout = misses[len(misses)-1]
+		mutbase = str(mu)
+		muts = mutbase.split(".")
+		mutout = muts[len(muts)-1]
+
+	
+		outfilename = infilebase + "-miss" + missout + "-mu" + mutout + "-" + str(rep) + ".fasta"
+		outfile = open(outfilename, 'w')
+		lenchk = -1
 
 
-for x in outseqs:
-	outfile.write(">")
-	outfile.write(x[0])
-	outfile.write("\n")
-	outfile.write(x[1])
-	outfile.write("\n")
-outfile.close()
+		for x in outseqs:
+			outfile.write(">")
+			outfile.write(x[0])
+			outfile.write("\n")
+			outfile.write(x[1])
+			outfile.write("\n")
+		outfile.close()
 
 
 
--- scripts/fastadiff.py
+++ scripts/fastadiff.py
@@ -5,12 +5,9 @@
     Author - Matthew Jobin
     """
 
-
-
 import argparse
 from argparse import RawTextHelpFormatter
-
-print "\n\n***FASTADIFF ***\n\n"
+import os
 
 parser = argparse.ArgumentParser(description="This script compares \n\n\t" \
                                              "- two FASTA files.\n\t" \
@@ -19,10 +16,19 @@ parser = argparse.ArgumentParser(description="This script compares \n\n\t" \
 
 parser.add_argument('-afile',metavar='<afile>',help='input file A: .fasta', required=True)
 parser.add_argument('-bfile',metavar='<bfile>',help='input file B: .fasta')
+parser.add_argument('-impoutfile',metavar='<impoutfile>',help='impout file')
+parser.add_argument('-silent', dest='silent', help='None of your beeswax.',
+					action='store_true')
+parser.set_defaults(silent=False)
 
 args = parser.parse_args()
 afile = args.afile
 bfile = args.bfile
+impoutfile = args.impoutfile
+silent = bool(args.silent)
+
+if not silent:
+	print "\n\n***FASTADIFF ***\n\n"
 
 twofile = True
 
@@ -30,17 +36,9 @@ if afile[-5:] != 'fasta':
     print "Error: FASTA files with a .fasta extension only."
     quit()
 
-if not bfile:
-	twofile = False
-	bfile = afile
-
-
-if bfile[-5:] != 'fasta':
-    print "Error: FASTA files with a .fasta extension only."
-    quit()
-
-	
-
+abasename = os.path.basename(afile)
+afilebase, afileext = os.path.splitext(abasename)
+    
 aseq = {}
 afile_data = open(afile, 'r')
 for file_line in afile_data:
@@ -49,63 +47,132 @@ for file_line in afile_data:
         data_line = next(afile_data).rstrip()
         aseq[id] = data_line
 
-print "Number of read sequences in file" , " " ,len(aseq)
+if not silent:
+	print "Number of read sequences in file" , " " ,len(aseq)
 
+imputedic = {}
 
-bseq = {}
-bfile_data = open(bfile, 'r')
-for file_line in bfile_data:
-    if file_line[:1] is '>':
-        id = file_line[1:].rstrip()
-        data_line = next(bfile_data).rstrip()
-        bseq[id] = data_line
-
-print "Number of read sequences in file" , " " ,len(aseq)
-
-
-totdiffs = 0
-totmissdiffs = 0
-totbases = 0
-seqdiffs = {}
-
-if twofile:
-#Comparison
-	for k in aseq:
-		if k in bseq:
-			print "****************** " , k , " **********************"
-			xaseq = aseq[k]
-			xbseq = bseq[k]
-			if len(xaseq) != len(xbseq):
-				print "Error: lines should be same size for sequences: ", k , " Skipping."
-				continue
-			diffs = [i for i in xrange(len(xaseq)) if xaseq[i] != xbseq[i]]
-			totdiffs += len(diffs)
-			totbases += len(xaseq)
-			seqdiffs[k] = (float(len(diffs)) / float(len(xaseq)))
-			for diff in diffs:
-				print xaseq[diff] , " : " , xbseq[diff]
-else:
-	for k in aseq:
-		for l in bseq:
-			if k == l:
-				continue
-			xaseq = aseq[k]
-			xbseq = bseq[l]
-			if len(xaseq) != len(xbseq):
-				print "Error: lines should be same size for sequences: ", k , " Skipping."
-				continue
-			diffs = [i for i in xrange(len(xaseq)) if xaseq[i] != xbseq[i]]
-			print k, "\t" , l, "\t", (float(len(diffs)) / float(len(xaseq)))
-			
+if impoutfile:
+	impoutfile_data = open(impoutfile, 'r')
+	for ifile_line in impoutfile_data:
+		impline = ifile_line.rstrip()
+		impcols = impline.split('\t')
+		impname = impcols[0] + "-" + impcols[1]
+		imputedic[impname] = ifile_line
 		
+
+outfilebase, outfileext = os.path.splitext(afile)
+outfilename =  outfilebase + "-pairwise.txt"
+outfile = open(outfilename, 'w')
+
+batchlist = []
+
+if not bfile:
+	print "not bfile"
+	twofile = False
+	bfile = afile
+	batchlist.append(afile)
+elif os.path.isdir(bfile):
+	for bbfile in os.listdir(bfile):
+		if bbfile.endswith(".fasta"):
+			batchlist.append(bfile + "/" + bbfile)
+elif os.path.isfile(bfile):
+	if bfile.endswith(".fasta"):
+		batchlist.append(bfile)
+	else:
+		print "Input file must be either .fasta or .vcf"
+		exit()
+else:
+	print "Input file must be either .fasta or .vcf"
+	exit()
+
+pairwises = []
+
+for bfile in batchlist:
+
+# 	mmimpouts = []
+	bbasename = os.path.basename(bfile)
+	bfilebase, bfileext = os.path.splitext(bbasename)
+	
+	bseq = {}
+	bfile_data = open(bfile, 'r')
+	for file_line in bfile_data:
+		if file_line[:1] is '>':
+			id = file_line[1:].rstrip()
+			data_line = next(bfile_data).rstrip()
+			bseq[id] = data_line
+	if not silent:
+		print "Number of read sequences in file" , " " ,len(bseq)
+
+
+	totdiffs = 0
+	totmissdiffs = 0
+	totbases = 0
+	seqdiffs = {}
+	diffsites = {}
+
+	if twofile:
+	#Comparison
+		for k in aseq:
+			if k in bseq:
+				xaseq = aseq[k]
+				xbseq = bseq[k]
+				if len(xaseq) != len(xbseq):
+					print "Error: lines should be same size for sequences: ", k , " Skipping."
+					continue
+				diffs = [i for i in xrange(len(xaseq)) if xaseq[i] != xbseq[i]]
+				totdiffs += len(diffs)
+				totbases += len(xaseq)
+				seqdiffs[k] = (float(len(diffs)) / float(len(xaseq)))
+				diffsites[k] = diffs
+	else:
+		for k in aseq:
+			for l in bseq:
+				if k == l:
+					continue
+				xaseq = aseq[k]
+				xbseq = bseq[l]
+				if len(xaseq) != len(xbseq):
+					print "Error: lines should be same size for sequences: ", k , " Skipping."
+					continue
+				diffs = [i for i in xrange(len(xaseq)) if xaseq[i] != xbseq[i]]
+				totdiffs += len(diffs)
+				totbases += len(xaseq)
+				seqdiffs[k] = (float(len(diffs)) / float(len(xaseq)))
+				print k + "\t" + l + "\t" + str((float(len(diffs)) / float(len(xaseq))))
+				diffsites[k] = diffs
+
 			
+	if not silent:
+		print "\n********* " , bfile		
+		print "ID\tDiffs"
+	for x in sorted(diffsites.keys()):
+		for tdiff in diffsites[x]:
+			diffname = x + "-" + str(tdiff)
+# 			if impoutfile:
+# 				mmimpouts.append(imputedic[diffname])
+		if len(diffsites[x]) > 0:
+			if not silent:
+				print x, "\t", diffsites[x]
+	if not silent:
+		print "Differences: ", totdiffs, " out of " , totbases
+	pairwise = float(totdiffs) / float(totbases)
+	if not silent:
+		print "Pairwise Distance: " , pairwise
+	outfile.write(str(pairwise))
+	pairwises.append(pairwise)
+	outfile.write("\n")
+# 	impoutoutname = afilebase + "-" +  bfilebase + "-mmimpout.txt"
+# 	impoutoutfile = open(impoutoutname, 'w')
+# 	impoutoutfile.write("SUBJECTID\t VAR\t FROM\t TO\t TYPE\tPASS\tIMPUTED\n")
+# 	for imputed in mmimpouts:
+# 		impoutoutfile.write(imputed)
+# 		impoutoutfile.write("\n")
+# 	impoutoutfile.close()
 
-
-print "ID\tDiff"
-for x in sorted(seqdiffs.keys()):
-	print x, "\t", seqdiffs[x]
-
-print "\nMean Difference: " , float(totdiffs) / float(totbases)
+if not silent:
+	print "Average Pairwise Distance for all infiles: "
+print sum(pairwises) / float(len(pairwises))
 
 
 
